408332334fd8e4c1bb1266049970e70e10a6dc0c
Ashish Verma <119559585+DevAshish9090@users.noreply.github.com>
2026-08-21 02:12:35 +0530

    Add: kth-distinct-string-in-an-array - submission-0

---
+++ Data Structures & Algorithms/kth-distinct-string-in-an-array/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        count = {}
+        for i in arr:
+            if i not in count:
+                count[i] = 1
+            else:
+                count[i] = count[i] + 1 
+                
+        for i in arr:
+            if count[i] == 1:
+                k = k-1
+                if k == 0:
+                    return i 
+        return ""            
+
